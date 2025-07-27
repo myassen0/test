02bf1fceb72349eac20bb0048079f8bccc266e10
Mahmoud Yassen <mahmoudyassen1005@gmail.com>
2025-07-28 01:40:23 +0300

    Update buildDockerImage.groovy

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -1,5 +1,4 @@
 def call(String imageName, String imageTag) {
     echo "Building Docker image: ${imageName}:${imageTag}"
-    sh "docker build -t ${imageName}:${imageTag} docker/"
+    sh "docker build -t ${imageName}:${imageTag} . -f docker/Dockerfile"
 }
-
