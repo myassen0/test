3ead7c76a2a67e5ffc445ba1a9b2deff323e3975
Mahmoud Yassen <mahmoudyassen1005@gmail.com>
2025-07-28 01:15:56 +0300

    Update pushDockerImage.groovy

--- vars/pushDockerImage.groovy
+++ vars/pushDockerImage.groovy
@@ -1,5 +1,14 @@
 def call(String imageName, String imageTag) {
     echo "Pushing Docker image: ${imageName}:${imageTag}"
-    sh "docker push ${imageName}:${imageTag}"
-}
 
+    withCredentials([usernamePassword(
+        credentialsId: 'dockerhub',
+        usernameVariable: 'DOCKER_USER',
+        passwordVariable: 'DOCKER_PASS'
+    )]) {
+        sh """
+            echo "\$DOCKER_PASS" | docker login -u "\$DOCKER_USER" --password-stdin
+            docker push ${imageName}:${imageTag}
+        """
+    }
+}
