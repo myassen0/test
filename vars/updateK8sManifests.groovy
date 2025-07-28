35901abdeba556018248fdf280ae3e1813f19799
Mahmoud Yassen <mahmoudyassen1005@gmail.com>
2025-07-28 04:53:14 +0300

    Update updateK8sManifests.groovy

--- vars/updateK8sManifests.groovy
+++ vars/updateK8sManifests.groovy
@@ -1,6 +1,6 @@
 def call(String imageName, String imageTag, String manifestPath) {
     echo "Updating Kubernetes manifests in ${manifestPath} with image ${imageName}:${imageTag}"
     sh """
-    find ${manifestPath} -name '*.yaml' -exec sed -i 's|image: .\\+|image: ${imageName}:${imageTag}|' {} +
+    find ${manifestPath} -name '*.yaml' -exec grep -q "image: ${imageName}:${imageTag}" {} \\; -exec echo "No update needed in {}" \\; -o -exec sed -i 's|image: .*|image: ${imageName}:${imageTag}|' {} +
     """
 }
