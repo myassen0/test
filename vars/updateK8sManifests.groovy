7a4a96e1ca4c2903c230bb322f9a3993b0040d58
Mahmoud Yassen <mahmoudyassen1005@gmail.com>
2025-07-28 05:50:07 +0300

    Update updateK8sManifests.groovy

--- vars/updateK8sManifests.groovy
+++ vars/updateK8sManifests.groovy
@@ -1,13 +1,16 @@
-def call(String imageTag) {
-    def filePath = 'k8s/deployment.yaml'
+def call(Map args) {
+    def imageTag = args.imageTag
+    def filePath = args.filePath
+    def containerName = args.containerName
+
     def content = readFile(filePath)
 
+    // يحدد الصورة الخاصة بالكونتينر المحدد فقط
     def updatedContent = content.replaceAll(
-        /(image:\s+.+:)([\w\.-]+)/,
-        { fullMatch, prefix, oldTag -> "${prefix}${imageTag}" }
+        /(name:\s*${containerName}.*?\n\s*image:\s*)([\w\./:-]+)/s,
+        { fullMatch, prefix, oldImage -> "${prefix}yassenn01/myapp:${imageTag}" }
     )
 
     writeFile(file: filePath, text: updatedContent)
-    echo "✅ Updated image tag to: ${imageTag}"
-
+    echo "✅ Updated image tag to: ${imageTag} for container: ${containerName}"
 }
