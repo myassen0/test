454d0d3bc804cd9bfb5b5d57f120b2da27ad99bf
Mahmoud Yassen <mahmoudyassen1005@gmail.com>
2025-07-30 17:14:30 +0300

    Update pushDockerImage.groovy

--- vars/pushDockerImage.groovy
+++ vars/pushDockerImage.groovy
@@ -1,14 +1,12 @@
-def call(String imageName, String imageTag) {
-    echo "Pushing Docker image: ${imageName}:${imageTag}"
+// vars/pushImage.groovy
+// Pushes a Docker image to a registry.
+// Parameters:
+//   config.repoName: The name of the Docker repository.
+//   config.imageTag: The tag of the Docker image.
+//   config.dockerRegistry: The URL of the Docker registry.
 
-    withCredentials([usernamePassword(
-        credentialsId: 'dockerhub',
-        usernameVariable: 'DOCKER_USER',
-        passwordVariable: 'DOCKER_PASS'
-    )]) {
-        sh """
-            echo "\$DOCKER_PASS" | docker login -u "\$DOCKER_USER" --password-stdin
-            docker push ${imageName}:${imageTag}
-        """
-    }
+def call(Map config) {
+    echo "Pushing Docker image ${config.dockerRegistry}/${config.repoName}:${config.imageTag} to registry..."
+    sh "docker push ${config.dockerRegistry}/${config.repoName}:${config.imageTag}"
+    echo "Docker image pushed successfully."
 }
