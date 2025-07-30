9897c7afa89113f2943f860cf2582ad429f459d6
Mahmoud Yassen <mahmoudyassen1005@gmail.com>
2025-07-30 17:14:04 +0300

    Update deleteLocalDockerImage.groovy

--- vars/deleteLocalDockerImage.groovy
+++ vars/deleteLocalDockerImage.groovy
@@ -1,5 +1,10 @@
-def call(String imageName, String imageTag) {
-    echo "Deleting local Docker image: ${imageName}:${imageTag}"
-    sh "docker rmi ${imageName}:${imageTag} || true"
-}
+// vars/deleteLocalImage.groovy
+// Deletes a Docker image from the local Docker daemon.
+// Parameters:
+//   config.fullImageName: The full name of the image to delete (e.g., 'myregistry/my-app:1.0.0').
 
+def call(Map config) {
+    echo "Deleting local Docker image ${config.fullImageName}..."
+    sh "docker rmi ${config.fullImageName}"
+    echo "Local Docker image deleted."
+}
